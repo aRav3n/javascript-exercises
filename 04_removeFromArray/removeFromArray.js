b73e4e38d1eac836fa8c0b685acd91380c3abb1e
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-01-23 10:30:29 +0530

    Code exercise 04 such that it removes a single item from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayIn, itemToRemove) {
+    let length = arrayIn.length;
+    let arrayOut = [];
 
+    // Populate arrayOut with all values that aren't items to remove
+    for (let i = 0; i < length; i++) {
+        if (arrayIn[i] !== itemToRemove) {
+            arrayOut[arrayOut.length] = arrayIn[i];
+        };
+    };
+    return arrayOut;
 };
 
 // Do not edit below this line

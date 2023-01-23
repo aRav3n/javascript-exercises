45008031f9028e6547c413e3a508aa79ce25f790
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-01-23 10:15:22 +0530

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(stringIn) {
+    let stringOut = '';
+    // Start at end of the string using charAt to populate stringOut
+    let lastChar = stringIn.length - 1;
+    for (let i = lastChar; i >= 0; i--) {
+        let char = stringIn.charAt(i);
+        stringOut += char;
+    };
+    // Return stringOut
+    return stringOut;
 };
 
 // Do not edit below this line

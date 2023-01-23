e59f4785c0261cb994a17e91f3c8bcc713a76923
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-01-23 09:57:31 +0530

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(stringIn, numberIn) {
+    let stringOut = '';
+    
+    // Verify that numberIn is >= 0
+    if (numberIn < 0) {
+        return 'ERROR';
+    };
 
+    // Loop through numberIn times
+    for (let i = 0; i < numberIn; i++) {
+        stringOut += stringIn;
+    };
+    return stringOut;
 };
 
 // Do not edit below this line

9d1a117a1e91707c48b81f481b81fb05b5bfc7b6
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-01-23 11:33:11 +0530

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(startNumber, endNumber) {
+    let sum = 0;
+    // Verify that numbers are legit and >= 0
+    if (typeof startNumber != 'number' || typeof endNumber != 'number' || startNumber < 0 || endNumber < 0) {
+        return 'ERROR';
+    };
+    if (startNumber > endNumber){
+        let temp = startNumber;
+        startNumber = endNumber;
+        endNumber = temp;
+    };
+    for (let i = startNumber; i <= endNumber; i++) {
+        sum += i;
+    };
+    return sum;
 };
 
 // Do not edit below this line

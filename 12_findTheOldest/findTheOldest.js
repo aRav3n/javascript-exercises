1e60c567958e4bb35c17cfe293624c0e0994c617
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-02-07 12:11:13 +0530

    Complete the exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPerson = {
+        name: "",
+        yearOfBirth: 1,
+        yearOfDeath: 1,
+    };
+    let length = people.length;
+    for(let i = 0; i < length; i++) {
+        let deathYearOfPerson;
+        if("yearOfDeath" in people[i]){
+            deathYearOfPerson = people[i].yearOfDeath;
+        } else {
+            deathYearOfPerson = new Date().getFullYear();
+            people[i].yearOfDeath = deathYearOfPerson;
+        };
+        let lifeSpanOfPerson = deathYearOfPerson - people[i].yearOfBirth;
+        let deathYearOfOldestPerson = oldestPerson.yearOfDeath;
+        let lifeSpanOfOldestPerson = deathYearOfOldestPerson - oldestPerson.yearOfBirth;
+        console.log({lifeSpanOfOldestPerson})
+        if(lifeSpanOfPerson > lifeSpanOfOldestPerson){
+            oldestPerson = people[i];
+        };
+        console.log({oldestPerson})
+    };
+    return oldestPerson;
 };
 
 // Do not edit below this line

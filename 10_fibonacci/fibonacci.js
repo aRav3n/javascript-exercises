ed6d73f0e5fb53e30595c1fb309c2d133046cd27
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-02-07 11:23:36 +0530

    Return Fibonacci number of input

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequencePosition) {
+    if(sequencePosition < 0) {
+        return "OOPS";
+    };
+    let fibonacciNumbers = [];
+    for(let i = 0; i < sequencePosition; i++){
+        if(i == 0 || i == 1){
+            fibonacciNumbers[i] = 1;
+        } else {
+            fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+        };
+    };
+    let answer = fibonacciNumbers[sequencePosition - 1];
+    return answer;
 };
 
 // Do not edit below this line

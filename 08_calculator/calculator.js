8b0ab5115b89a756d1262a1ac343fe91e2a504dc
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-02-07 11:25:16 +0530

    Create calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+	let answer = firstNumber +  secondNumber;
+  return answer;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	let answer = firstNumber - secondNumber;
+  return answer;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let answer = 0;
+  for(let number of numbers) {
+    answer += number;
+  };
+  return answer;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let answer = 1;
+  for(let number of numbers) {
+    answer *= number;
+  };
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(baseNumber, raisedToThe) {
+	let answer = 1;
+  for(let i = 0; i < raisedToThe; i++) {
+    answer *= baseNumber;
+  };
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialOf) {
+  if(factorialOf === 0 || factorialOf === 1) {
+    return 1;
+  } else if(factorialOf > 1) {
+    return factorialOf *= factorial(factorialOf - 1);
+  };
 };
 
 // Do not edit below this line

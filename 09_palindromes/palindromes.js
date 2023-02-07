2994a6069f7a8d21f359eafd49cd852f6756403a
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-02-07 11:24:43 +0530

    Check if string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (inputWord) {
 
+    // Convert all letters to lower case
+    let lowerCaseWord = inputWord.toLowerCase();
+    // Remove all non letters
+    let initialLength = lowerCaseWord.length;
+    let convertedInputWord = "";
+    for(let i = 0; i < initialLength; i++) {
+        if(lowerCaseWord.charAt(i) != lowerCaseWord.charAt(i).toUpperCase()) {
+            convertedInputWord += lowerCaseWord.charAt(i);
+        };
+    };
+    // Check if is palindrome
+    const wordLength = convertedInputWord.length;
+    for(let i = 0; i < wordLength; i++) {
+        if(convertedInputWord.charAt(i) != convertedInputWord.charAt(wordLength - 1 - i)) {
+            return false;
+        };
+    };
+    return true;
 };
 
 // Do not edit below this line
